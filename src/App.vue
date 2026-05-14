845484bd771edf094f761a8b5967b9799f79174a
j0t4inc0de <juan.erices04@inacapmail.cl>
2026-05-13 20:54:54 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="bg-red-500 text-white p-4 text-center">
+    Hola Mundo, este sera un dashboard de IoT con Vue.js y Tailwind CSS
+  </div>
+</template>
+
+<style scoped></style>
